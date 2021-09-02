e6e8091c77883b03cf0525f1ad10a0d18e9b9e0c
zhibisora <2134923900@qq.com>
2021-09-03 05:29:16 +0800

    Some minor fixes have been made to adjust favicon.
    Favicon changed from ICO format to SVG format to ensure compatibility on iGEM server.

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        // Members: resolve(__dirname, '/Members/index.html'),
+        Members: resolve(__dirname, 'Members/index.html'),
         Modeling: resolve(__dirname, 'Modeling/index.html')
       }
     }
