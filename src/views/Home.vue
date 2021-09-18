cade8536af55a5737b952776774fc12438a60301
zhibisora <2134923900@qq.com>
2021-09-18 23:10:51 +0800

    tools change

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -31,9 +31,111 @@
                             style="background-image: url(https://2021.igem.org/wiki/images/9/9e/T--NJMU-China--1.2.png)"
                         ></div>
                     </div>
-                    <div
-                        class="letter"
-                    >As a medical college, students in Nanjing Medical University often actively participate in voluntary activities such as the COVID-19 nucleic acid testing.</div>
+                    <div class="letter">{{ content[0] }}</div>
+                </div>
+                <div></div>
+                <div></div>
+            </section>
+            <section class="sect3 text-left" :style="{ opacity: value2 }">
+                <div class="grid gap-x-4 cell3 h-full" style="grid-template-columns: 3fr 7fr;">
+                    <div class="letter">{{ content[1] }}</div>
+                    <div class="letter bg-white rounded-2xl overflow-hidden box">
+                        <div
+                            class="h-full absolute w-full rounded-2xl bg-cover bg-center"
+                            style="background-image: url(https://2021.igem.org/wiki/images/d/d8/T--NJMU-China--2.1.png)"
+                        ></div>
+                    </div>
+                </div>
+                <div></div>
+                <div></div>
+            </section>
+            <section class="sect3 text-left" :style="{ opacity: value2 }">
+                <div class="grid gap-x-4 cell2 h-full">
+                    <div class="letter bg-white rounded-2xl overflow-hidden box">
+                        <div
+                            class="h-full absolute w-full rounded-2xl bg-cover bg-center"
+                            style="background-image: url(https://2021.igem.org/wiki/images/e/e5/T--NJMU-China--1.1.png)"
+                        ></div>
+                    </div>
+                    <div class="letter">{{ content[2] }}</div>
+                </div>
+                <div></div>
+                <div></div>
+            </section>
+            <section class="sect3 text-left" :style="{ opacity: value2 }">
+                <div class="grid gap-x-4 cell3 h-full">
+                    <div class="letter">{{ content[3] }}</div>
+                    <div class="letter bg-white rounded-2xl overflow-hidden box">
+                        <div
+                            class="h-full absolute w-full rounded-2xl bg-cover bg-center"
+                            style="background-image: url(https://2021.igem.org/wiki/images/e/e5/T--NJMU-China--1.1.png)"
+                        ></div>
+                    </div>
+                </div>
+                <div></div>
+                <div></div>
+            </section>
+            <section class="sect3 text-left" :style="{ opacity: value2 }">
+                <div class="grid gap-x-4 cell2 h-full">
+                    <div class="letter bg-white rounded-2xl overflow-hidden box">
+                        <div
+                            class="h-full absolute w-full rounded-2xl bg-cover bg-center"
+                            style="background-image: url(https://2021.igem.org/wiki/images/e/e5/T--NJMU-China--1.1.png)"
+                        ></div>
+                    </div>
+                    <div class="letter">{{ content[4] }}</div>
+                </div>
+                <div></div>
+                <div></div>
+            </section>
+            <section class="sect3 text-left" :style="{ opacity: value2 }">
+                <div class="grid gap-x-4 cell3 h-full">
+                    <div class="letter">{{ content[5] }}</div>
+                    <div class="letter bg-white rounded-2xl overflow-hidden box">
+                        <div
+                            class="h-full absolute w-full rounded-2xl bg-cover bg-center"
+                            style="background-image: url(https://2021.igem.org/wiki/images/e/e5/T--NJMU-China--1.1.png)"
+                        ></div>
+                    </div>
+                </div>
+                <div></div>
+                <div></div>
+            </section>
+            <section class="sect3 text-left" :style="{ opacity: value2 }">
+                <div class="grid gap-x-4 cell2 h-full">
+                    <div class="letter bg-white rounded-2xl overflow-hidden box">
+                        <div
+                            class="h-full absolute w-full rounded-2xl bg-cover bg-center"
+                            style="background-image: url(https://2021.igem.org/wiki/images/e/e5/T--NJMU-China--1.1.png)"
+                        ></div>
+                    </div>
+                    <div class="letter">{{ content[6] }}</div>
+                </div>
+                <div></div>
+                <div></div>
+            </section>
+            <section class="sect3 text-left" :style="{ opacity: value2 }">
+                <div class="grid gap-x-4 cell3 h-full" style="grid-template-columns: 4fr 6fr;">
+                    <div class="letter">{{ content[7] }}</div>
+                    <div class="letter bg-white rounded-2xl overflow-hidden box">
+                        <div
+                            class="h-full absolute w-full rounded-2xl bg-cover bg-center"
+                            style="background-image: url(https://2021.igem.org/wiki/images/e/e5/T--NJMU-China--1.1.png)"
+                        ></div>
+                    </div>
+                </div>
+                <div></div>
+                <div></div>
+            </section>
+            <section class="sect3 text-left" :style="{ opacity: value2 }">
+                <div class="grid gap-x-4 cell2 h-full">
+                    <div class="letter bg-white rounded-2xl overflow-hidden box">
+                        <div
+                            class="h-full absolute w-full rounded-2xl bg-cover bg-center"
+                            style="background-image: url(https://2021.igem.org/wiki/images/e/e5/T--NJMU-China--1.1.png)"
+                        ></div>
+                    </div>
+                    <div class="letter">{{ content[8] }}</div>
                 </div>
                 <div></div>
                 <div></div>
@@ -51,6 +153,18 @@ import { gsap } from "gsap"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
 
 gsap.registerPlugin(ScrollTrigger)
+
+
+let content = ['As medical students of Nanjing Medical University, we often actively participate in voluntary activities such as the COVID-19 nucleic acid testing.',
+    'Last year, we went to Star Of The Sea（海之星）to do volunteer activities. Because of this opportunity in such an educational institution specially for autistic children, we got to know a group of special kids - autistic children',
+    'After in-depth understanding, we discovered that autism is one of the fastest-growing developmental disability. According to American CDC, prevalence of autism in the U.S. has increased from 1 in 68 births in 2010 to 1 in 54 births in 2020. And in China the statistic is about 1 in 100 births.',
+    'On May 14, 2021, SJTU & Science released the booklet 125 Questions: Exploration and Discovery. The problem what is the etiology of autism indicated that autism has aroused strong social concern.',
+    'In contact with these children, we found that this set of heterogeneous neurodevelopmental conditions are characterised by early-onset difficulties in social communication and unusually restricted, repetitive behaviour and interests.',
+    'In our hometown-China, children with ASDs are also what we call Children From the Star. Because they are like stars twinkling in the distant sky, lonely and silent but twinkling on their own planets.',
+    'Last year, we carried out the project “Sound of Silence and won the Gold Award. We proposed a domestic autism screening method based on urinary metabolites in view of problems of screening in autism.',
+    'This year, NJMU-China team completely upgraded the project. We constructed the project of the prevention and treatment of autism children. In more detail, we focus on maternal prevention and offspring treatment of autism caused by maternal infection during pregnancy.',
+    'Our project aims at building up a stairway to the starlight, using engineering probiotics to prevent autism caused by maternal infection during pregnancy and treat children, thus structuring a way to health for the ‘Children From the Star’.',
+]
 
 onMounted(() => {
     window.addEventListener('scroll', handleScroll)
@@ -105,32 +219,27 @@ let ae = () => {
     body.style.zIndex = "0"
 }
 // sect2 滚动逻辑
-let sect2: HTMLElement = null!
 let box: HTMLElement = null!
-let bigBody: HTMLElement = null!
 let sect2Img2: HTMLElement = null!
 let sect2Img3: HTMLElement = null!
 let sect2Letters: NodeListOf<HTMLElement> = null!
 onMounted(() => {
-    window.addEventListener('scroll', sect2Scroll, { passive: false })
-    sect2 = document.querySelector('.body')!
+    window.addEventListener('scroll', sect2Scroll)
     box = document.querySelector('.box')!
-    bigBody = document.querySelector('.bigBody')!
     sect2Img2 = document.querySelector('.img2')!
     sect2Img3 = document.querySelector('.img3')!
     sect2Letters = document.querySelectorAll('.sect2 .letter')
     sect2ImgChange(100 + box.clientWidth + 'px')
 })
 let sect2Scroll = (e: Event) => {
+
+    // gsap.to
     let height = window.innerHeight / 2
 
     let elScroll = window.scrollY - height * 2
     if (elScroll > 0 && elScroll < 2 * height) {
-        // e.preventDefault()
-
         sect2Letters[0].style.transform = "translateY(" + elScroll + 'px' + ')'
         sect2Letters[1].style.transform = "translateY(" + elScroll + 'px' + ')'
-        // bigBody.style.top = elScroll + 'px'
 
         let Img2Scroll = 100 + box.clientWidth - 2 * elScroll
         if (!(Img2Scroll < -5)) {
@@ -149,6 +258,10 @@ let sect2ImgChange = (pos: string) => {
 </script>
 
 <style scoped>
+/* 用于修复nav变化时层
+/* 用于修复nav变化时层
+/* 用于修复nav变化时层级在.sect2和.div之下的bug */
+/* 然而混合模式无法修复，
 /* 用于修复nav变化时层级在.sect2和.div之下的bug */
 /* 然而混合模式无法修复，导致依旧很奇怪 一劳永逸的解决办法就是不要让它在转变的时候就碰到文字 */
 /* 等把文字框的padding设置起来应该就可以删掉了，当然，保守起见，如果不碍事，造成很大麻烦的话，可以继续保留 */
@@ -182,6 +295,16 @@ let sect2ImgChange = (pos: string) => {
     padding: 150px 100px;
     position: relative;
 }
+.sect3 {
+    height: 100vh;
+    padding: 150px 100px;
+    position: relative;
+    transition: background-color 0.2s;
+}
+.sect2:hover,
+.sect3:hover {
+    background-color: rgba(0, 0, 0, 0.144);
+}
 /* 用于修复nav变化时层级在.sect2和.div之下的bug (旧版) */
 /* .sect2 {
     z-index: -1;
@@ -195,7 +318,7 @@ let sect2ImgChange = (pos: string) => {
     justify-content: center;
     align-items: center;
     /* font-size: 1.5rem; */
-    font-size: 4vh;
+    font-size: 3.5vh;
     padding: 20px;
     color: white;
     line-height: 1.5;
@@ -207,6 +330,17 @@ let sect2ImgChange = (pos: string) => {
     grid-template-columns: 7fr 3fr;
     grid-template-rows: 5fr 5fr;
     row-gap: 200px;
+}
+.cell2 {
+    grid-template-columns: 7fr 3fr;
+    /* grid-template-rows: 5fr 5fr; */
+    /* row-gap: 200px; */
+}
+
+.cell3 {
+    grid-template-columns: 3fr 7fr;
+    /* grid-template-rows: 5fr 5fr; */
+    /* row-gap: 200px; */
 }
 @media screen and (orientation: portrait) {
     .cell {
